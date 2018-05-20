892be77ad8e333deb609077b3f1d35a1880e5482
Ayase <137056643@qq.com>
2018-05-21 07:13:30 +0800

    move sql file.

--- cmake/after_ws_install.cmake
+++ cmake/after_ws_install.cmake
@@ -3,13 +3,11 @@ if( WIN32 )
     add_custom_command(TARGET worldserver
       POST_BUILD
       COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_AHBOT_DIR}/conf/mod_ahbot.conf.dist" ${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/
-	  COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_AHBOT_DIR}/sql/world.auctionhousebot.sql" ${CMAKE_SOURCE_DIR}/data/sql/custom/db_world/
     )
   elseif ( MINGW )
     add_custom_command(TARGET worldserver
       POST_BUILD
       COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_AHBOT_DIR}/conf/mod_ahbot.conf.dist" ${CMAKE_BINARY_DIR}/bin/
-	  COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_AHBOT_DIR}/sql/world.auctionhousebot.sql" ${CMAKE_SOURCE_DIR}/data/sql/custom/db_world/
     )
   endif()
 endif()
