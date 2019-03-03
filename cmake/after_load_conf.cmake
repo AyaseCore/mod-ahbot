c0e52095903e7adbd5eecfdfe622fba03b26b8a9
ayase <137056643@qq.com>
2019-03-03 08:37:16 +0800

    fix typo

--- cmake/after_load_conf.cmake
+++ cmake/after_load_conf.cmake
@@ -1,2 +1,2 @@
 add_definitions(-DMOD_AH_BOT)
-AC_ADD_CONFIG_FILE("${CMAKE_MOD_ELUNA_ENGINE_DIR}/conf/mod_LuaEngine.conf.dist")
+AC_ADD_CONFIG_FILE("${CMAKE_MOD_AHBOT_DIR}/conf/mod_ahbot.conf.dist")
