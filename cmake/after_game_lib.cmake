0592394c9b54b3a9907afdf39c7ff39cab03078e
Kargatum <dowlandtop@yandex.com>
2019-04-14 03:56:09 +0700

    Build: Fix build in actual AC (#5)
    * Build: Fix build in actual AC
    * Move all to scripts

--- cmake/after_game_lib.cmake
+++ cmake/after_game_lib.cmake
@@ -1,1 +1,11 @@
-include_directories(${CMAKE_MOD_AHBOT_DIR}/src)
+#
+#
+#
+
+CollectIncludeDirectories(
+  ${CMAKE_MOD_AHBOT_DIR}/src
+  PUBLIC_INCLUDES)
+
+target_include_directories(game-interface
+  INTERFACE
+    ${PUBLIC_INCLUDES})
--- cmake/after_load_conf.cmake
+++ cmake/after_load_conf.cmake
@@ -1,2 +1,6 @@
+#
+#
+#
+
+# Add definitions
 add_definitions(-DMOD_AH_BOT)
-AC_ADD_CONFIG_FILE("${CMAKE_MOD_AHBOT_DIR}/conf/mod_ahbot.conf.dist")
